fa1d8a6eebc85a33739a0e1419f654bcf32ce657
Arthur Santiago <arthursantaguiar@gmail.com>
2025-06-17 17:35:36 -0300

    feat: adding infrastructure dependency injection

---
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Enums;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infrastructure;
+public static class DependencyInjectionExtension
+{
+
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var dataBaseType = configuration.GetConnectionString("DatabaseType");
+
+        var databaseTypeEnum = (DataBaseType)Enum.Parse(typeof(DataBaseType), dataBaseType!);
+
+        if(databaseTypeEnum == DataBaseType.MySql)
+        {
+            AddDbContext_MySql(services, configuration);
+        }
+        else
+        {
+            AddDbContext_SqlServer(services, configuration);
+        }
+
+        AddRepositories(services);
+    }
+
+    private static void AddDbContext_SqlServer(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ConnectionSqlServer");
+        services.AddDbContext<MyRecipeBookDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+    }
+
+    private static void AddDbContext_MySql(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ConnectionMySql");
+        var serverVersion = new MySqlServerVersion(new Version(9,0,1));
+
+        services.AddDbContext<MyRecipeBookDbContext>(options =>
+        {
+            options.UseMySql(connectionString, serverVersion);
+        });
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUnityOfWork, UnityOfWork>();
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        services.AddScoped<IUserWriteOnlyRepository , UserRepository>();
+    }
+}
