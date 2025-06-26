b23921dd6aa83c3c83beb21fec7991192f69540d
Arthur Santiago <arthursantaguiar@gmail.com>
2025-06-25 21:25:51 -0300

    feat: adding migratiosn and database configurations

---
+++ src/Backend/MyRecipeBook.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Infrastructure.Extensions;
+public static class ConfigurationExtensions
+{
+    public static bool IsUnitTestEnviroment(this IConfiguration configuration)
+    {
+        return configuration.GetValue<bool>("InMemoryTest");
+    }
+
+    public static string ConnectionString(this IConfiguration configuration)
+    {
+        var databaseType = configuration.DatabaseType();
+
+        if(databaseType == DataBaseType.MySql)
+            return configuration.GetConnectionString("ConnectionMySql")!;
+        else
+            return configuration.GetConnectionString("ConnectionSqlServer")!;
+    }
+
+    public static DataBaseType DatabaseType(this IConfiguration configuration)
+    {
+        var dataBaseType = configuration.GetConnectionString("DatabaseType");
+
+        return (DataBaseType)Enum.Parse(typeof(DataBaseType), dataBaseType!);
+
+    }
+}
