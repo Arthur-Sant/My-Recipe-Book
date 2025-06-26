832edc9ca144fb6dd060f6affa026ef251c184be
Arthur Santiago <arthursantaguiar@gmail.com>
2025-06-25 21:40:16 -0300

    test: adding integration test for register user

---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,65 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+using System.Globalization;
+
+namespace WebApi.Test.User.Register;
+
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public RegisterUserTest(CustomWebApplicationFactory factory) => _httpClient = factory.CreateClient();
+
+    [Fact]
+    public async Task Success()
+    {
+        var body = RequestRegisterUserJsonBuilder.Build();
+
+        var response = await _httpClient.PostAsJsonAsync("User", body);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+        await using var resposeBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(resposeBody);
+
+        responseData.RootElement.GetProperty("name").GetString().ShouldSatisfyAllConditions(
+            v => v.ShouldBe(body.Name),
+            v => v.ShouldNotBeNullOrWhiteSpace()
+            );
+    }
+
+    [Theory]
+    [ClassData(typeof(CultueInlineDataTest))]
+    public async Task Error_Empty_Name(string culture)
+    {
+        var body = RequestRegisterUserJsonBuilder.Build();
+        body.Name = string.Empty;
+
+        if(_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
+            _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
+
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
+
+        var response = await _httpClient.PostAsJsonAsync("User", body);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        await using var resposeBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(resposeBody);
+
+        var erros = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+        erros.ShouldSatisfyAllConditions(
+            v => v.ShouldHaveSingleItem(),
+            v => v.ShouldContain(e => e.GetString()!.Equals(expectedMessage))
+            );
+    }
+}
