4779c97a5a0e42785a4e5775b0645f3e54c17be1
Arthur Santiago <arthursantaguiar@gmail.com>
2025-07-11 07:57:31 -0300

    test: adding logged user unit test

---
+++ tests/UseCases.Test/User/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,32 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using MyRecipeBook.Application.UseCases.User.Profile;
+using Shouldly;
+
+namespace UseCases.Test.User.Profile;
+public class GetUserProfileUseCaseTest
+{
+
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var useCase = CreateUsecase(user);
+
+        var result = await useCase.Execute();
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Email.ShouldBe(user.Email);
+    }
+
+    private static IGetUserProfileUseCase CreateUsecase(MyRecipeBook.Domain.Entities.User user)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var mapper = MapperBuilder.Build();
+
+        return new GetUserProfileUseCase(loggedUser, mapper);
+    }
+}
