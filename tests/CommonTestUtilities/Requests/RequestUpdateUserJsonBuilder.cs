f31424b2e1985099e99bbd8cd12f77a9a3e5ca4b
Arthur Santiago <arthursantaguiar@gmail.com>
2025-07-14 15:32:52 -0300

    test: adding user update unit test

---
+++ tests/CommonTestUtilities/Requests/RequestUpdateUserJsonBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestUpdateUserJsonBuilder
+{
+    public static RequestUpdateUserJson Build()
+    {
+        return new Faker<RequestUpdateUserJson>()
+            .RuleFor(user => user.Name, f => f.Person.FirstName)
+            .RuleFor(user => user.Email, (f, u) => f.Internet.Email(u.Email));
+    }
+}
