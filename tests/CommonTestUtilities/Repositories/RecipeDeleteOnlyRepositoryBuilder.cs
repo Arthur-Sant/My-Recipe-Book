4d43059194ae08722efdd8e97d77a8fe6c845992
Arthur Santiago <arthursantaguiar@gmail.com>
2025-07-21 01:54:15 -0300

    test: adding recipe deletion and reading repository

---
+++ tests/CommonTestUtilities/Repositories/RecipeDeleteOnlyRepositoryBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories.Recipe;
+
+namespace CommonTestUtilities.Repositories;
+
+public class RecipeDeleteOnlyRepositoryBuilder
+{
+    public static IRecipeDeleteOnlyRepository Build() => new Mock<IRecipeDeleteOnlyRepository>().Object;
+}
