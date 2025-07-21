df100ad3954884bfb96a4f4ddf4244bd2677f836
Arthur Santiago <arthursantaguiar@gmail.com>
2025-07-21 02:13:20 -0300

    test: adding delete recipe e2e test

---
+++ tests/WebApi.Test/Recipe/Delete/DeleteRecipeTest.cs
@@ -0,0 +1,67 @@
+﻿using CommonTestUtilities.IdEncripter;
+using CommonTestUtilities.Tokens;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Recipe.Delete;
+
+public class DeleteRecipeTest : MyRecipeBookClassFixture
+{
+    private readonly string _route = "recipe", 
+        _recipeId;
+
+    private readonly Guid _userIdentifier;
+
+    public DeleteRecipeTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+        _recipeId = factory.GetRecipeId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var response = await DoDelete(
+            route: $"{_route}/{_recipeId}", 
+            token: token
+            );
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        response = await DoGet($"{_route}/{_recipeId}", token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultueInlineDataTest))]
+    public async Task Error_Recipe_Not_Found(string culture)
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var id = IdEncripterBuilder.Build().Encode(1000);
+
+        var response = await DoDelete(
+            route: $"{_route}/{_recipeId}",
+            token: token,
+            culture: culture
+            );
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+
+        var errors = await GetErrosFromResponse(response);
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("RECIPE_NOT_FOUND", new CultureInfo(culture))!;
+
+        errors.ShouldSatisfyAllConditions(
+            v => v.ShouldHaveSingleItem(),
+            v => v.ShouldContain(e => e.GetString()!.Equals(expectedMessage))
+            );
+    }
+}
