11e89eeced690d18e3f2fc208e3dbc0de1e8e27c
Arthur Santiago <arthursantaguiar@gmail.com>
2025-07-31 19:17:32 -0300

    test: adding generate recipe builders

---
+++ tests/CommonTestUtilities/DTOs/GeneratedRecipeDTOBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using MyRecipeBook.Domain.DTOs;
+using MyRecipeBook.Domain.Enums;
+
+namespace CommonTestUtilities.DTOs;
+
+public class GeneratedRecipeDTOBuilder
+{
+    public static GeneratedRecipeDTO Build()
+    {
+        return new Faker<GeneratedRecipeDTO>()
+            .RuleFor(r => r.Title, faker => faker.Lorem.Word())
+            .RuleFor(r => r.CookingTime, faker => faker.PickRandom<CookingTime>())
+            .RuleFor(r => r.Ingredients, faker => faker.Make(1, () => faker.Commerce.ProductName()))
+            .RuleFor(r => r.Instructions, faker => faker.Make(1, () => new GeneratedInstructionDTO
+            {
+                Step = 1,
+                Text = faker.Lorem.Paragraph()
+            }));
+    }
+}
