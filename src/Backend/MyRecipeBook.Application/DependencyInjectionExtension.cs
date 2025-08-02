eae2019d015e7c7354f16027a3e40dc61716b95d
Arthur Santiago <arthursantaguiar@gmail.com>
2025-08-01 22:49:49 -0300

    feat: adding image recipe use case

--- src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -8,6 +8,7 @@ using MyRecipeBook.Application.UseCases.Recipe.Delete;
 using MyRecipeBook.Application.UseCases.Recipe.Filter;
 using MyRecipeBook.Application.UseCases.Recipe.Generate;
 using MyRecipeBook.Application.UseCases.Recipe.GetById;
+using MyRecipeBook.Application.UseCases.Recipe.Image;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Application.UseCases.Recipe.Update;
 using MyRecipeBook.Application.UseCases.User.ChangePassword;
@@ -63,6 +64,7 @@ public static class DependencyInjectionExtension
         services.AddScoped<IDeleteRecipeUseCase, DeleteRecipeUseCase>();
         services.AddScoped<IUpdateRecipeUseCase, UpdateRecipeUseCase>();
         services.AddScoped<IGenerateRecipeUseCase, GenerateRecipeUseCase>();
+        services.AddScoped<IAddUpdateImageCoverUseCase, AddUpdateImageCoverUseCase>();
 
         services.AddScoped<IGetDashboardUseCase, GetDashboardUseCase>();
     }
