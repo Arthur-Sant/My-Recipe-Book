3bd6991f1faeca5b4e894468a23beeaf1b2f27b9
Arthur Santiago <arthursantaguiar@gmail.com>
2025-08-07 07:14:59 -0300

    feat: changing the code generation method making it more secure

--- src/Backend/MyRecipeBook.Domain/ValueObjects/MyRecipeBookRuleConstants.cs
+++ src/Backend/MyRecipeBook.Domain/ValueObjects/MyRecipeBookRuleConstants.cs
@@ -5,4 +5,5 @@ public abstract class MyRecipeBookRuleConstants
     public const int MAXIMUM_INGREDIENTS_GENERATE_RECIPE = 5;
     public const int MAXIMUM_IMAGE_URL_LIFETIME_IN_MINUTES = 10;
     public const int PASSWORD_RESET_CODE_VALIDITY_MINUTES = 10;
+    public const int NUMBER_CHARACTERS_FOR_ACTION_CODE = 6;
 }
