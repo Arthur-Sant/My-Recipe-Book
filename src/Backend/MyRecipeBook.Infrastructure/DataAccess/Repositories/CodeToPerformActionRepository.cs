ab102ecbb9ea940ce9899002e02a9cacb370f01c
Arthur Santiago <arthursantaguiar@gmail.com>
2025-08-07 04:54:04 -0300

    feat: adding action code repository

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/CodeToPerformActionRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.CodeToPerformAction;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+public class CodeToPerformActionRepository(
+    MyRecipeBookDbContext _dbContext
+    ) : ICodeToPerformActionReadOnlyRepository, ICodeToPerformActionWriteOnlyRepository, ICodeToPerformActionDeleteOnlyRepository
+{
+    public async Task Add(CodeToPerformAction codeToPerformAction) => await _dbContext.CodeToPerformActions.AddAsync(codeToPerformAction);
+
+    public async Task DeleteAllUserCodes(long userId)
+    {
+        await _dbContext.CodeToPerformActions
+            .Where(c => c.UserId.Equals(userId))
+            .ExecuteDeleteAsync();
+    }
+
+    public async Task<bool> ExistCode(string code) => await _dbContext.CodeToPerformActions.AnyAsync(c => c.Value.Equals(code));
+
+    public async Task<CodeToPerformAction?> GetByCode(string code)
+    {
+        return await _dbContext.CodeToPerformActions
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Value.Equals(code));
+    }
+}
