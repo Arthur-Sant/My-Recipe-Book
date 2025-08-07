cc3da75d8a489be1d0124699aab768b5889ef6d6
Arthur Santiago <arthursantaguiar@gmail.com>
2025-08-07 04:51:52 -0300

    feat: adding create table action code migration

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseVersions.cs
@@ -5,4 +5,5 @@ public abstract class DatabaseVersions
     public const int TABLE_USERS = 1;
     public const int TABLE_RECIPES = 2;
     public const int IMAGE_FOR_RECIPES = 3;
+    public const int TABLE_CODE_TO_PERFORM_ACTIONS = 4;
 }
---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/Versions/Verison0000004.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_CODE_TO_PERFORM_ACTIONS, "Create table to save a code when the user forgot password")]
+public class Verison0000004 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("CodeToPerformActions")
+            .WithColumn("Value").AsString().NotNullable()
+            .WithColumn("UserId").AsInt64().NotNullable().ForeignKey("FK_CodeToPerformActions_User_Id", "Users", "Id");
+    }
+}
