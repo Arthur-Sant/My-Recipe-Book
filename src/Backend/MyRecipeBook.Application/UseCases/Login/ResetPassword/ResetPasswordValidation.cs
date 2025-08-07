0edb509ed73c8ecae6ebc1da16fd45f3b344223c
Arthur Santiago <arthursantaguiar@gmail.com>
2025-08-07 07:17:15 -0300

    feat: adding validation for the number of characters in the code when resetting the password

--- src/Backend/MyRecipeBook.Application/UseCases/Login/ResetPassword/ResetPasswordValidation.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/Login/ResetPassword/ResetPasswordValidation.cs
@@ -3,6 +3,9 @@ using MyRecipeBook.Application.Shared.Validators;
 using MyRecipeBook.Communication.Requests.Login;
 using MyRecipeBook.Communication.Requests.User;
 using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.ValueObjects;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.Login.ResetPassword;
 
@@ -11,5 +14,9 @@ public class ResetPasswordValidation : AbstractValidator<RequestResetPasswordJso
     public ResetPasswordValidation()
     {
         RuleFor(user => user.NewPassword).SetValidator(new PasswordValidator<RequestResetPasswordJson>());
+        RuleFor(user => user.Code)
+            .NotEmpty()
+            .Length(MyRecipeBookRuleConstants.NUMBER_CHARACTERS_FOR_ACTION_CODE)
+            .WithMessage(ResourceMessagesException.CODE_INVALID);
     }
 }
