2b9b24aa7a7808e1b1fd3e86a9e974551f63e836
Arthur Santiago <arthursantaguiar@gmail.com>
2025-08-11 09:51:29 -0300

    test: adding refresh token generator builder

---
+++ tests/CommonTestUtilities/Tokens/RefreshTokenGeneratorBuilder.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Application.UseCases.Token.Refresh;
+using MyRecipeBook.Domain.Security.Tokens;
+using MyRecipeBook.Infrastructure.Security.Tokens.Acess.Generator;
+using MyRecipeBook.Infrastructure.Security.Tokens.Refresh;
+
+namespace CommonTestUtilities.Tokens;
+
+public class RefreshTokenGeneratorBuilder
+{
+    public static IRefreshTokenGenerator Build() => new RefreshTokenGenerator();
+
+}
